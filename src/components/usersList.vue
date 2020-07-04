e16440fd0fc07c65a628a17ee3fa992f10d612a6
leanlimac <leandrolima7472@gmail.com>
2020-07-04 14:29:38 -0300

    Handle if there's nothing to show | if one user is selected

--- src/components/usersList.vue
+++ src/components/usersList.vue
@@ -1,19 +1,48 @@
 <template>
     <div>
-        <userItem v-for="user of Users" :key="user.id"
-        :User="user"/>
-    </div>
+        <div v-if="userSelected != ''" >
+            <userItem @click.native="clean()"
+                :User="userSelected">
+                {{userSelected}}
+            </userItem>
+        </div>
+        <div v-else-if="Users.length >= 1 && userSelected == ''">
+            <userItem v-for="user of Users" :key="user.id"
+            :User="user"
+            @click.native="userClicked(user)"/>
+        </div>
+        <div v-else>
+           <p>Nada para listar</p>
+        </div>
+    </div> 
 </template>
 
 <script>
 import userItem from '@/components/userItem'
 
 export default {
+    data(){
+        return{
+            userSelected: '',
+        }
+    },
     props:{
         Users: {}
     },
     components:{
         userItem
+    },
+    computed:{
+    },
+    methods:{
+        userClicked(user){
+            console.log(this.userSelected);
+            this.userSelected = user;
+        },
+        clean(){
+            console.log('clean user')
+            this.userSelected = '';
+        }
     }
 }
 </script>
