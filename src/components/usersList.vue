b2395e380603aa9b3ede01d38a0518deb1077ed1
leanlimac <leandrolima7472@gmail.com>
2020-07-04 13:03:54 -0300

    Users list receives data from father - Testing

---
+++ src/components/usersList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <tr v-for="user of Users" :key="user.id">
+            <td>
+                <img :src="user.avatar_url">{{user}}</td>
+            <br>
+        </tr> 
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        Users: {}
+    }
+}
+</script>
+
+<style>
+
+</style>
