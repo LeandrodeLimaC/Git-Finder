3d86f787f3c70b5b1da9bf9c605fa3d599506313
leanlimac <leandrolima7472@gmail.com>
2020-07-04 17:45:26 -0300

    Specified headers and credential use

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: 'https://api.github.com/search'
+    baseURL: 'https://api.github.com',
+    withCredentials: false,
+    headers:{
+        Accept:"application/json",
+        "Content-Type": "application/json"
+    }
 })
