285055549a80373ece667384ae5365eea67c401b
leanlimac <leandrolima7472@gmail.com>
2020-07-04 12:07:35 -0300

    Searchbar Component emits DebouncedQuery to his father

---
+++ src/components/searchBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <input v-model="searchQuery" type="text" name="" id="">
+</template>
+
+<script>
+// Helpers
+import { debounce } from '@/helpers/debounce'
+
+export default {
+    data() {
+        return {
+            searchQuery: '',
+        }
+    },
+    watch: {
+        searchQuery: 
+            debounce(function (debouncedQuery) {
+                this.emitQuery(debouncedQuery)
+            }, 500)
+    },
+    methods:{
+        emitQuery(debouncedQuery){
+            if(debouncedQuery.length > 0){
+                this.$emit("debouncedQuery", debouncedQuery)
+            }
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
