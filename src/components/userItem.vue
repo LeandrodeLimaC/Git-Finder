daa1667e867a2d3b790116f9c665f00ed1ac05c2
leanlimac <leandrolima7472@gmail.com>
2020-07-04 13:09:56 -0300

    Receive user data

---
+++ src/components/userItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <img :src="User.avatar_url">{{User}}
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        User: {}
+    }
+}
+</script>
+
+<style>
+
+</style>
