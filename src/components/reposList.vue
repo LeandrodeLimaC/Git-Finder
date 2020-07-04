31a3f7110b26ef84374d22b05d720f85f55da2a5
leanlimac <leandrolima7472@gmail.com>
2020-07-04 17:46:54 -0300

    Created & Tested Ropositories list component

---
+++ src/components/reposList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div >
+        <div v-if="userRepos.length >= 1">
+            <div v-for="repo in userRepos" v-bind:key="repo.id">
+                {{repo}}
+            </div>
+        </div>
+        
+        <div v-else>
+           <p>Este usuário nao tem repositórios</p>
+        </div>
+  </div>    
+</template>
+
+<script>
+export default {
+    props:{
+        userRepos: {}
+    },
+}
+</script>
+
+<style>
+
+</style>
