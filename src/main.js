feea014d6c33bf1ad1d2a2738c0e47293e47f0b9
leanlimac <leandrolima7472@gmail.com>
2020-07-07 15:23:10 -0300

    Import and Use of Vue material

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdButton, MdContent, MdTabs, MdList, MdIcon, MdDivider, MdAvatar, MdField, MdApp, MdEmptyState, MdProgress} from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
+Vue.use(MdList);
+Vue.use(MdDivider);
+Vue.use(MdIcon);
+Vue.use(MdAvatar);
+Vue.use(MdField);
+Vue.use(MdApp);
+Vue.use(MdEmptyState);
+Vue.use(MdProgress);
 
 new Vue({
   render: h => h(App),
